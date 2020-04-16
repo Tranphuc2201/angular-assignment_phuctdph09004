29baaa0935ebb5738dd39003a64376c6fb6522bf
Tranphuc2201 <phuchien2201@gmail.com>
2020-04-16 14:31:14 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { AdminComponent } from './admin/admin.component';
 // import { ProductManager } from './product-manager/product-manager.component';
 @NgModule({
   imports:      [ BrowserModule,AppRoutingModule,FormsModule , HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductComponent, ProductDetailComponent, ProductAddComponent, AboutComponent, HomeComponent, NotFoundComponent, ProductEditComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductComponent, ProductDetailComponent, ProductAddComponent, AboutComponent, HomeComponent, NotFoundComponent, ProductEditComponent, AdminComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
