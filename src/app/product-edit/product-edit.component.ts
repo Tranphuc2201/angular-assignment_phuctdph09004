306368075814e5076681069cf3d132cc11887cba
Tranphuc2201 <phuchien2201@gmail.com>
2020-04-21 15:48:28 +0700

    1 file modified

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Product } from "../Product";
-import{ProductService} from "../product.service";
+import{ProductService} from "../product.service"
+// import { ProductService } from "../service/product.service";
 import { Router, ActivatedRoute, Route } from "@angular/router";
-
+import { from } from 'rxjs';
+import { data } from '../MockData';
 @Component({
   selector: "app-product-edit",
   templateUrl: "./product-edit.component.html",
   styleUrls: ["./product-edit.component.css"]
 })
 export class ProductEditComponent implements OnInit {
-  product: Product;
+  product: Product ;
+  id: number;
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
   ) {}
+  sub: any;
 
   ngOnInit() {
    this.getProduct();
   }
-  
-   getProduct() {
-    this.route.params.subscribe(param => {this.productService.getProduct(param.productID).
-    subscribe(data => (this.product = data));
-    });
+  getProduct() {
+    this.route.params.subscribe(param =>{
+      this.productService.getProductdetail(param.productID).subscribe(data =>{
+          this.product = data;
+      // this.productService.getProducts(param.productID).subscribe(data =>{
+      //   this.product = data;
+    })
+  })
   }
-UpdateProduct() {
-    // console.log(this.product);
-    this.productService.updateProduct(this.product).subscribe(data => {
-      this.router.navigateByUrl("/admin/dashboard");
-      
-    });
-    alert("Bạn đã update thành công!");
-  }
+  updateProduct(){
+this.productService.updateProduct(this.product).subscribe(data =>{
+this.router.navigateByUrl('/admin/dashboard');
 
+})
+
+  }
 }
